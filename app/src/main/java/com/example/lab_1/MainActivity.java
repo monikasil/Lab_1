ed27bdac291123ceef280b43ac04d94d099a79bb
Monika <monika.siliene@knf.stud.vu.lt>
2021-04-19 00:32:04 +0300

    Revert "Added text change to Button"
    This reverts commit 5109dbb3

--- app/src/main/java/com/example/lab_1/MainActivity.java
+++ app/src/main/java/com/example/lab_1/MainActivity.java
@@ -3,23 +3,13 @@ package com.example.lab_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView TextView_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.TextView_1 = findViewById(R.id.textView_1);
-    }
-
-    public void OnButtonclick(View view) {
-        this.TextView_1.setText("You've change the text!");
     }
 
     public void OnButtonclick_2(View view) {
